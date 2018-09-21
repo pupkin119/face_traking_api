e489bfc7fd5b10ae246540aef921a4313bec8442
Alex <pupkin11999@gmail.com>
2018-09-21 12:18:03 +0300

    add api with settings

--- face_traking_api/settings.py
+++ face_traking_api/settings.py
@@ -143,8 +143,8 @@ DATABASES = {
     #     }
     # },
 }
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+## db_from_env = dj_database_url.config(conn_max_age=500)
+## DATABASES['default'].update(db_from_env)
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
